5b93d285deb57254e79498df9050d45332b2184c
masaya sato <masataro1201@gmail.com>
2026-04-23 12:49:48 +0900

    課題Q2の回答を追加

--- question/app.js
+++ question/app.js
@@ -3,3 +3,9 @@ let nickname = 'ごっしー';
 let  age = '28';
 
 console.log('私のニックネームは' + nickname + 'です。年齢は' + age + '歳です。');
+
+//Q2 配列
+let  languages = ['JavaScript', 'PHP', 'Ruby', 'Python', 'Go'];
+
+console.log(`私の好きな言語は${languages[0]}です。次は${languages[3]}を勉強してみたいです。`);
+
